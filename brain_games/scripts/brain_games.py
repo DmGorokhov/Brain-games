79f7b04d5be9749a4c6b02861ec07be578d7475c
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-11-23 18:42:26 +0300

    lint-clean cli.py and brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,9 +2,11 @@
 
 import brain_games.cli
 
+
 def main():
     print('Welcom to the Brain Games!')
     brain_games.cli.welcome_user()
 
-if __name__ ==  '__main__':
+
+if __name__ == '__main__':
     main()
