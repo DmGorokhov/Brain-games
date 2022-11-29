ff82b614e2c729d937b2eef1074b6ccfd3051b1d
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-11-29 23:52:14 +0300

    add brain_calc.py to /scripts

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.games.calc_game as calc_game
+import brain_games.games_engine as engine
+
+
+def main():
+    engine.run_game(calc_game)
+
+
+if __name__ == '__main__':
+    main()
