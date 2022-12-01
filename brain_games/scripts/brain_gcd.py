b0387694a574bd5cf96a8a4432435136c20845b8
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-01 15:50:20 +0300

    add brain_gcd.py to /scripts

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.games.gcd_game as gcd_game
+import brain_games.games_engine as engine
+
+
+def main():
+    engine.run_game(gcd_game)
+
+
+if __name__ == '__main__':
+    main()
