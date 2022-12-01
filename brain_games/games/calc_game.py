5346998ce2776d5dea46980631d2ecc9ef3d811c
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-01 15:52:03 +0300

    modified calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,23 +1,24 @@
-import random
+from random import choice, choices
 
 game_question = 'What is the result of the expression?'
 
 
-def calculate_expession():
-    number1, number2 = random.choices(range(30), k=2)
-    operation = random.choice(['+', '-', '*'])
-
-    expression = f'{number1} {operation} {number2}'
-
+def calculate_expession(number1, number2, operation):
     if operation == '+':
-        correct_answer = str(number1 + number2)
+        cacl_result = number1 + number2
     elif operation == '-':
-        correct_answer = str(number1 - number2)
+        cacl_result = number1 - number2
     else:
-        correct_answer = str(number1 * number2)
+        cacl_result = number1 * number2
 
-    return expression, correct_answer
+    return cacl_result
 
 
 def result():
-    return calculate_expession()
+    number1, number2 = choices(range(30), k=2)
+    operation = choice(['+', '-', '*'])
+
+    expression = f'{number1} {operation} {number2}'
+    correct_answer = str(calculate_expession(number1, number2, operation))
+
+    return expression, correct_answer
