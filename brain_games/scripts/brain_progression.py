19a6261f9fdb978036c704cffef9fdfefb296616
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-01 22:07:32 +0300

    add brain_progression.py to /scripts

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.games.progression_game as progression_game
+import brain_games.games_engine as engine
+
+
+def main():
+    engine.run_game(progression_game)
+
+
+if __name__ == '__main__':
+    main()
