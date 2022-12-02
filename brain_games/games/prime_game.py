4d9192804fdbfe51324f52cf07f6df3d3f2f6983
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-02 17:30:40 +0300

    add prime_game.py to /games

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,21 @@
+from random import randint
+
+game_question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    if number % 2 == 0:
+        return False
+    divisor = 3
+    while divisor * divisor <= number and number % divisor != 0:
+        divisor += 2
+    return divisor * divisor > number
+
+
+def result():
+    number = randint(1, 200)
+    if is_prime(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return number, correct_answer
