8bddd678843be2bbfa277d396f3985851f08aa06
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-02 17:31:28 +0300

    add brain_prime.py to /scripts

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import brain_games.games.prime_game as prime_game
+import brain_games.games_engine as engine
+
+
+def main():
+    engine.run_game(prime_game)
+
+
+if __name__ == '__main__':
+    main()
