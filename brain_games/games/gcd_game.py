bbd35a552acd9186d315b6def25e279c3fdb44f3
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-04 10:35:54 +0300

    modified gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -3,24 +3,24 @@ from random import randint
 GAME_QUESTION = 'Find the greatest common divisor of given numbers.'
 
 
-def get_gcd(number1, number2):
+def get_gcd(first_number: int, second_number: int) -> int:
     """gcd means greatest common divisor"""
 
-    while number1 != 0 and number2 != 0:
-        if number1 > number2:
-            number1 = number1 % number2
+    while first_number != 0 and second_number != 0:
+        if first_number > second_number:
+            first_number = first_number % second_number
         else:
-            number2 = number2 % number1
+            second_number = second_number % first_number
 
-    greatest_common_divisor = number1 + number2
+    greatest_common_divisor = first_number + second_number
     return greatest_common_divisor
 
 
-def result():
-    number1 = randint(2, 100)
-    number2 = randint(2, 100)
+def get_game():
+    random_number_1 = randint(2, 100)
+    random_number_2 = randint(2, 100)
 
-    question = f'{number2} {number1}'
-    correct_answer = str(get_gcd(number1, number2))
+    question = f'{random_number_1} {random_number_2}'
+    correct_answer = str(get_gcd(random_number_1, random_number_2))
 
     return question, correct_answer
