47ca5640d9ed785ce3e9d489daa81cd980d848e6
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-04 10:37:25 +0300

    modified progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,18 +4,18 @@ GAME_QUESTION = 'What number is missing in the progression?'
 
 
 def get_progression():
-    lenght = randint(5, 10)
+    length = randint(5, 10)
     start_value = randint(1, 80)
     step = randint(1, 9)
-    progression = list(range(start_value, start_value + (lenght * step), step))
+    progression = list(range(start_value, start_value + (length * step), step))
 
     return progression
 
 
-def result():
+def get_game():
     progression = get_progression()
-    missing_number = choice(progression)
-    progression[progression.index(missing_number)] = '..'
+    hidden_number = choice(progression)
+    progression[progression.index(hidden_number)] = '..'
     question = ' '.join([str(item) for item in progression])
 
-    return question, str(missing_number)
+    return question, str(hidden_number)
