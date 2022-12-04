67cb48f2df0fbaa0687511e43b3efdcb356e2baa
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-04 10:36:29 +0300

    modified iseven_game.py

--- brain_games/games/iseven_game.py
+++ brain_games/games/iseven_game.py
@@ -3,14 +3,14 @@ from random import randint
 GAME_QUESTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(number):
+def is_even(number: int):
     if number % 2 == 0:
         return True
     else:
         return False
 
 
-def result():
+def get_game():
     random_number = randint(1, 1000)
     if is_even(random_number):
         correct_answer = 'yes'
