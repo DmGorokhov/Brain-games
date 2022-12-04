f40532c80be6322ba4b4f732265f21fc4c3d234f
Dmitry Gorokhov <gorokhov86@gmail.com>
2022-12-04 10:36:56 +0300

    modified prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -3,7 +3,7 @@ from random import randint
 GAME_QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def is_prime(number):
+def is_prime(number: int):
     if number % 2 == 0:
         return False
     divisor = 3
@@ -12,10 +12,10 @@ def is_prime(number):
     return divisor * divisor > number
 
 
-def result():
-    number = randint(1, 200)
-    if is_prime(number):
+def get_game():
+    number_for_question = randint(1, 200)
+    if is_prime(number_for_question):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    return number, correct_answer
+    return number_for_question, correct_answer
